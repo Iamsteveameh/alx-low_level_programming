da3c0f1d885372e8570e51407faad67f72190352
Iamsteveameh <atagastephen2@gmail.co>
2022-07-05 13:09:42 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers
+ * @var1: first integer
+ * @var2: second integer
+ *
+ * Created by: Iamsteveameh
+ * cc: 5th july, 2022
+ *
+ * Return: the integer sum of var1 and var2
+ */
+
+int add(int var1, int var2)
+{
+	return (var1 + var2);
+}
