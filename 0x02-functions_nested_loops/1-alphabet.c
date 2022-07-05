31f14ad7def494f522103bff079c3f9c77d1b40f
Iamsteveameh <atagastephen2@gmail.co>
2022-07-05 09:30:53 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case
+ *
+ * created : Iamsteveameh
+ * cc: 5th july, 2022
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int ii;
+
+	for (ii = 'a'; ii <= 'z'; ii++)
+		_putchar(ii);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/mains/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: ALways 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
