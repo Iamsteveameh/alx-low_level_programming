45f130851680b2ba88609f97807f41c3c806f6ab
Iamsteveameh <atagastephen2@gmail.co>
2022-07-05 12:56:58 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - every minute of the day of jack bauer
+ *
+ * Created - Iamsteveameh
+ * cc: 5th july, 2022
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
