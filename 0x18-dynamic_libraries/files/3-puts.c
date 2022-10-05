7529effea6c1190665259f000a189a69aee603d9
Iamsteveameh <atagastephen2@gmail.com>
2022-10-04 18:43:56 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/files/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
